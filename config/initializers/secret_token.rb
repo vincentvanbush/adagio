df6472689ce3938a12dd63cc852908774fac3d6c
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2015-01-30 18:55:16 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Adagio::Application.config.secret_key_base = 'f4109485e16cf2fd004d9e3ad0654162da79c41ab0dd2aad07c41584577beeeb0bfd858d8f4149d60cee53b8f286cb1fdc09abf888a0a3895a1018e1001328c3'
