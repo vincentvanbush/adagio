931210b71bae3e485c8b4374c25c9b7ad2f9d6fa
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2015-02-01 23:34:43 +0100

    Validations on creating a bid

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -7,12 +7,29 @@ class Bid < ActiveRecord::Base
   validates :auction, presence: true
 
   validate :no_bid_yet, if: :instant_auction?
+  validate :is_best_bid, on: :create
+  validate :cannot_outbid_own_bid, on: :create
+
+  private
 
   def instant_auction?
     auction.auction_type == 'instant'
   end
 
   def no_bid_yet
-    errors.add(:auction, "Auction bids should be limited to 1 for an instant") if auction.bids.count > 0
+    errors.add(:auction, 'should have up to one bid') if auction.bids.count > 0
+  end
+
+  def is_best_bid
+    if price <= (auction.bids.maximum('price') || auction.price)
+      errors.add(:auction, 'bid should beat the initial price and the best bid')
+    end
+  end
+
+  def cannot_outbid_own_bid
+    best_bid = auction.bids.order('price desc').first
+    if best_bid != nil && user == best_bid.user
+      errors.add(:auction, 'cannot outbid your own bid')
+    end
   end
 end
