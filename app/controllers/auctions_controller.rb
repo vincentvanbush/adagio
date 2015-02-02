c289d0a00c077a0e2874b75984c4b0029399613c
Michal Buszkiewicz <buszkiewiczm@gmail.com>
2015-02-02 19:42:47 +0100

    Auction index view shows the title of their scope

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -1,14 +1,9 @@
 class AuctionsController < ApplicationController
   expose_decorated :auction
-  expose_decorated :auctions do
-    if params['user_id']
-      obj = User.find_by(id: params['user_id']).auctions
-    elsif params['category_id']
-      Category.find_by(id: params['category_id']).auctions
-    end
-  end
+  expose_decorated(:auctions) { parent_object.auctions }
   expose :category
   expose :user
+  expose(:parent) { parent_str }
 
   def new
   end
@@ -31,5 +26,23 @@ class AuctionsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def parent_object
+    if params['user_id']
+      User.find_by(id: params['user_id'])
+    elsif params['category_id']
+      Category.find_by(id: params['category_id'])
+    end
+  end
+
+  def parent_str
+    if parent_object.class == User
+      parent_object.email
+    elsif parent_object.class == Category
+      parent_object.title
+    end
   end
 end
